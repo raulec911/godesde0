75b841d031cabc2840e175126841151f374b85d3
Raúl Sánchez Campos <raulec911@hotmail.com>
2023-06-17 07:44:00 -0500

    Siguiente Commit

--- main.go
+++ main.go
@@ -3,11 +3,31 @@ package main
 import (
 	"fmt"
 
-	"github.com/raulec911/godesde0/variables"
+	"github.com/raulec911/godesde0/ejercicios"
 )
 
 func main() {
-	estado, texto := variables.ConviertoaTexto(100)
+	/*estado, texto := variables.ConviertoaTexto(100)
 	fmt.Println(estado)
+	fmt.Println(texto)
+	os := runtime.GOOS
+
+	if os == "linux" || os == "OS X." {
+		fmt.Println("Esto no es Windows, es ", os)
+	} else {
+		fmt.Println("Esto es Windows")
+	}
+
+	switch os {
+	case "linux":
+		fmt.Println("Esto es linux")
+	case "darwin":
+		fmt.Println("Esto es darwin")
+	default:
+		fmt.Printf("%s", os)
+	}*/
+
+	numero, texto := ejercicios.ConvNumerico("500A")
+	fmt.Println(numero)
 	fmt.Println(texto)
 }
