af5d225654a9f01fa16dace692907bb3b2516b50
Raúl Sánchez Campos <raulec911@hotmail.com>
2023-06-17 07:07:51 -0500

    Siguiente Commit

--- main.go
+++ main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/raulec911/godesde0/variables"
 )
 
 func main() {
-	variables.MuestroEnteros()
+	estado, texto := variables.ConviertoaTexto(100)
+	fmt.Println(estado)
+	fmt.Println(texto)
 }
