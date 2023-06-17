f99ac3711f61326a7e87996d37d55537ad2f6941
Raúl Sánchez Campos <raulec911@hotmail.com>
2023-06-17 06:33:58 -0500

    Primer Commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/raulec911/godesde0
+
+go 1.20
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/raulec911/godesde0/variables"
+)
+
+func main() {
+	variables.MuestroEnteros()
+}
